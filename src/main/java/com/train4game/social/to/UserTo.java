d7583011b0665b2d996e1d99caaccdd3e10deebc
Munoon <munoongg@gmail.com>
2019-09-08 11:47:27 +0300

    Add user registration

---
+++ src/main/java/com/train4game/social/to/UserTo.java
@@ -0,0 +1,71 @@
+package com.train4game.social.to;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserTo {
+    Integer id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    String name;
+
+    @NotBlank
+    @Size(max = 200)
+    String email;
+
+    @NotBlank
+    @Size(min = 5, max = 100)
+    String password;
+
+    public UserTo() {
+    }
+
+    public UserTo(Integer id, @NotBlank @Size(min = 2, max = 100) String name, @NotBlank @Size(max = 200) String email, @NotBlank @Size(min = 5, max = 100) String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
