4c73b8816d3adc000c99074aec51693ef1667a21
Munoon <munoongg@gmail.com>
2019-09-08 13:54:52 +0300

    Refactor configuration imports

--- src/main/java/com/train4game/social/config/SpringSecurityInitializer.java
+++ src/main/java/com/train4game/social/config/SpringSecurityInitializer.java
@@ -1,7 +1,9 @@
 package com.train4game.social.config;
 
+import org.springframework.context.annotation.Import;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+@Import(WebSecurityConfig.class)
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
