7a26cf4ce581c0df8df403d7107bfa9341ae8be9
Ruslan <shakirovusa@gmail.com>
2019-09-17 22:28:29 +0600

    Add NotFoundException

---
+++ src/main/java/com/train4game/social/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.train4game.social.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
