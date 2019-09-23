2783287d2bae57a6c21eea82c0d879af836d7a9b
Munoon <munoongg@gmail.com>
2019-09-23 23:31:48 +0300

    Refactor dropdown menu in navigation bar

--- src/main/webapp/resources/script.js
+++ src/main/webapp/resources/script.js
@@ -1,22 +1,6 @@
 const registrationEl = document.getElementById("registrationEl");
 const loginEl = document.getElementById("loginEl");
 const loginForm = document.forms['loginForm'];
-const dropdownMenu = document.getElementById('dropdown-menu');
-let isNavMenuOpen = false;
-
-if (dropdownMenu !== null) {
-    dropdownMenu.querySelector('button').addEventListener('click', e => {
-        e.preventDefault();
-        let elementClassList = dropdownMenu.querySelector('.dropdown-menu').classList;
-        elementClassList.toggle('show');
-        isNavMenuOpen = elementClassList.contains('show');
-    });
-
-    document.addEventListener('click', e => {
-        if (isNavMenuOpen)
-            dropdownMenu.querySelector('.dropdown-menu').classList.remove('show');
-    });
-}
 
 function showRegister() {
     registrationEl.hidden = false;
