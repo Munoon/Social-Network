c025af66d981000c7b95ec0e72fe62e795336f87
Munoon <munoongg@gmail.com>
2019-09-23 23:14:13 +0300

    Small refactoring in navigation bar 2

--- src/main/webapp/resources/script.js
+++ src/main/webapp/resources/script.js
@@ -2,12 +2,21 @@ const registrationEl = document.getElementById("registrationEl");
 const loginEl = document.getElementById("loginEl");
 const loginForm = document.forms['loginForm'];
 const dropdownMenu = document.getElementById('dropdown-menu');
+let isNavMenuOpen = false;
 
-if (dropdownMenu !== null)
+if (dropdownMenu !== null) {
     dropdownMenu.querySelector('button').addEventListener('click', e => {
         e.preventDefault();
-        dropdownMenu.querySelector('.dropdown-menu').classList.toggle('show');
+        let elementClassList = dropdownMenu.querySelector('.dropdown-menu').classList;
+        elementClassList.toggle('show');
+        isNavMenuOpen = elementClassList.contains('show');
     });
+
+    document.addEventListener('click', e => {
+        if (isNavMenuOpen)
+            dropdownMenu.querySelector('.dropdown-menu').classList.remove('show');
+    });
+}
 
 function showRegister() {
     registrationEl.hidden = false;
