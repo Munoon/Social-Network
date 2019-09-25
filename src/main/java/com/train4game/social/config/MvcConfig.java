45dac483bff13e5eb3c62680741f7fdcf9365566
Ruslan <shakirovusa@gmail.com>
2019-09-26 01:07:53 +0600

    Add LocalValidatorFactoryBean and its messageSource

--- src/main/java/com/train4game/social/config/MvcConfig.java
+++ src/main/java/com/train4game/social/config/MvcConfig.java
@@ -3,12 +3,16 @@ package com.train4game.social.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.train4game.social.addons.DatabaseLocaleResolver;
 import com.train4game.social.web.json.JacksonObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -23,6 +27,9 @@ import java.util.Locale;
 @Configuration
 @ComponentScan(basePackages = "com.train4game.social.**.web")
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Bean
     @Description("Jackson Object Mapper")
@@ -64,5 +71,12 @@ public class MvcConfig implements WebMvcConfigurer {
                 .resourceChain(false);
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
+    }
+
+    @Override
+    public Validator getValidator() {
+        final var validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource);
+        return validator;
     }
 }
