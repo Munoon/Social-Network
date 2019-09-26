41bdce7195677b7227b99ff4c5e6f9ee8b9dda98
Munoon <munoongg@gmail.com>
2019-09-26 09:43:13 +0300

    Refactor application properties for test working

--- README.md
+++ README.md
@@ -7,19 +7,18 @@
 # Instalation
 1) Set environment variable `SOCIAL_ROOT` with link on root folder of project.
 2) Setup PostgreSQL 11 or more.
-3) Configure settings in `src/main/resources/settings.properties` and `src/main/resources/db/postgres.properties` **OR** setup tomcat properties:
+3) Create database `social` and user for it with login `user` and password `password`
+4) Configure settings in `src/main/resources/settings.properties` and `src/main/resources/db/postgres.properties` **OR** setup tomcat properties:
 
     Property | Description
     -------- | -----------
-    postgresUsername | Database username
-    postgresPassword | Database password
     email | Email login
     password | password login
     recaptchaSecret | Recaptcha Secret code
     recaptchaHtml | Recaptcha HTML code
 
-4) To launch app you need Maven and Java 11.
-5) Run in root folder commands:
+5) To launch app you need Maven and Java 11.
+6) Run in root folder commands:
     ```
     $ mvn package
     $ mvn cargo:run
--- src/test/java/com/train4game/social/AbstractTest.java
+++ src/test/java/com/train4game/social/AbstractTest.java
@@ -3,12 +3,23 @@ package com.train4game.social;
 import com.train4game.social.config.AppConfig;
 import com.train4game.social.config.MvcConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
-@SpringJUnitWebConfig(classes = {AppConfig.class, MvcConfig.class})
+@SpringJUnitWebConfig(classes = {AppConfig.class, MvcConfig.class, AbstractTest.TestConfiguration.class})
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ExtendWith(TimingExtension.class)
 public abstract class AbstractTest {
+    @Configuration
+    static class TestConfiguration {
+        @Bean
+        JavaMailSender javaMailSender() {
+            return new JavaMailSenderImpl();
+        }
+    }
 }
